5b2f3a88145b0d4f3120a1032420ab924b0410e3
Anna Yakubovich <yakubovichannie@gmail.com>
2025-03-05 03:17:28 +0300

    [+] table styles added

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import './Table.style.scss';
 
 const Table = () => {
   return (
-    <div>
-      TABLE
-    </div>
+    <>
+    <table className="table">
+      <thead className="table__header">
+        <tr className="table__row">
+          <th className="table__header-cell">Уровень</th>
+          <th className="table__header-cell">Наименование работ</th>
+          <th className="table__header-cell">Основная з/п</th>
+          <th className="table__header-cell">Оборудование</th>
+          <th className="table__header-cell">Накладные расходы</th>
+          <th className="table__header-cell">Сметная прибыль</th>
+        </tr>
+      </thead>
+      <tbody className="table__body">
+      </tbody>
+    </table>
+    </>
   )
 }
 
